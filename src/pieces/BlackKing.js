c2e4e2c9e6da8b1639ae5c8b61918134eb111a9a
David Slaugh <dslaugh@gmail.com>
2019-07-26 14:45:38 -0600

    Fixed illegal castling moves

--- src/pieces/BlackKing.js
+++ src/pieces/BlackKing.js
@@ -41,8 +41,10 @@ export default function () {
 
 		// Castling moves
 		if (selectedSquare.occupant.hasMoved === false) {
-			const ooSquaresEmpty = [squares[5], squares[6]].every(square => !square.occupant);
-			if (ooSquaresEmpty && squares[7].occupant && squares[7].occupant.hasMoved === false) {
+			const ooSquares = [squares[5], squares[6]];
+			const ooSquaresNotAttacked = ooSquares.every(square => !squareIsAttacked(square.coords, squaresAttackedByWhite));
+			const ooSquaresEmpty = ooSquares.every(square => !square.occupant);
+			if (ooSquaresNotAttacked && ooSquaresEmpty && squares[7].occupant && squares[7].occupant.hasMoved === false) {
 				legalMoves.push({
 					x: selectedSquare.coords.x + 2,
 					y: selectedSquare.coords.y,
@@ -52,8 +54,10 @@ export default function () {
 				});
 			}
 
-			const oooSquaresEmpty = [squares[1], squares[2], squares[3]].every(square => !square.occupant);
-			if (oooSquaresEmpty && squares[0].occupant && squares[0].occupant.hasMoved === false ) {
+			const oooSquares = [squares[1], squares[2], squares[3]];
+			const oooSquaresNotAttacked = oooSquares.every(square => !squareIsAttacked(square.coords, squaresAttackedByWhite));
+			const oooSquaresEmpty = oooSquares.every(square => !square.occupant);
+			if (oooSquaresNotAttacked && oooSquaresEmpty && squares[0].occupant && squares[0].occupant.hasMoved === false ) {
 				legalMoves.push({
 					x: selectedSquare.coords.x - 2,
 					y: selectedSquare.coords.y,
